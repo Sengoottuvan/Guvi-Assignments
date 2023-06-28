c0e99286167bd8520becbd8ed3d376719d39b270
Sengoottuvan <david777sengu@gmail.com>
2023-06-28 20:29:49 +0530

    initial commit

---
+++ 01-04-2023/script.js
@@ -0,0 +1,57 @@
+let myResume={
+    "basics": {
+      "name": "S.Sengoottuvan",
+      "email": "sengoottuvan@gmail.com",
+      "phone": 8056224232,
+      "degree": "B.E",
+      "location": {
+        "address": "14/106, Kalavai Street, MountRoad",
+        "postalCode": 600002,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Sengoottuvan"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "The APA Enterprises PVT",
+        "position": "Business Executive",
+        "startDate": "2020-11-04",
+        "endDate": "2021-08-15",
+        "summary": "I was the business executive handling claims, enquiries and orders with the customers and dealers",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Thangavelu College Of Engineering",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2013,
+        "batch end year": 2017,
+        "gpa": 6.66,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginner",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "Hobbies": "Books, Movies",
+      }
+    ]
+  }
+  console.log(myResume);
+  
